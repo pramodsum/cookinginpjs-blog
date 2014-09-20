579629e69240f4a877b71201f6f90d8c337b8b73
Sumedha <pramods@umich.edu>
2014-09-20 18:14:27 -0400

    adding zurb foundation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,15 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create!(params[:comment])
+    @comment = @post.comments.create!(comment_params)
     respond_to do |format|
       format.html { redirect_to @post }
       format.js
     end
   end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
 end
