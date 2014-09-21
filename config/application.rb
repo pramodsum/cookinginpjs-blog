d2a86cd6dc6c32991510b423ebe88a909faa481f
Sumedha <pramods@umich.edu>
2014-09-21 19:56:13 -0400

    assets.compile

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,6 @@ module Foodblog
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.precompile += [
-      'common.css.scss'
-    ]
+    config.assets.compile = true
   end
 end
