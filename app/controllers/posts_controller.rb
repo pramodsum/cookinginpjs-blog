ee68b89ee10d45bf84678e802ac520c2ddea8d8a
Sumedha <pramods@umich.edu>
2014-09-25 12:06:46 -0400

    Fixed comments styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.order("created_at").all
   end
 
   # GET /posts/1
