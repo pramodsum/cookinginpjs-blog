8256fe3e9144008b2fe9e6e1505fd5b0c75109ce
Sumedha <pramods@umich.edu>
2015-02-07 08:58:21 +0530

    added eating out tag page

---
+++ content/themes/cookinginpjs/assets/js/maps.js
@@ -0,0 +1,38 @@
+var locations = [
+  ['Frita Batidos', "http://localhost:2368/whoop-whoop-whoop/", 42.280377, -83.74942],
+  ['Seoul Street', "", 42.298377, -83.7197],
+];
+
+var map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 10,
+  center: new google.maps.LatLng(42.2814, -83.7483),
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+});
+
+var infowindow = new google.maps.InfoWindow();
+
+var marker, i;
+var geocoder = geocoder = new google.maps.Geocoder();;
+var bounds = new google.maps.LatLngBounds();
+
+for (i = 0; i < locations.length; i++) {  
+  marker = new google.maps.Marker({
+    position: new google.maps.LatLng(locations[i][2], locations[i][3]),
+    map: map,
+    title: locations[i][0]
+  });
+
+  //add marker to bounds
+  bounds.extend(marker.getPosition());
+
+  google.maps.event.addListener(marker, 'click', (function(marker, i) {
+    return function() {
+      var content = "<a href=\"" + locations[i][1] + "\">" + locations[i][0] + "</a>"
+      infowindow.setContent(content);
+      infowindow.open(map, marker);
+    }
+  })(marker, i));
+}
+
+// recenter map to fit all markers
+map.fitBounds(bounds);
