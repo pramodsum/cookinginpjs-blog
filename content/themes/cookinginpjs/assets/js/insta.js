d58731af44e9b2730e8e8af09bc53a617fe43658
Sumedha <pramods@umich.edu>
2016-01-07 23:11:36 -0800

    Removing red

--- content/themes/cookinginpjs/assets/js/insta.js
+++ content/themes/cookinginpjs/assets/js/insta.js
@@ -5,7 +5,7 @@
     accessToken: '2462667315.467ede5.6a8150827314439e9d18ba4c52596886',
     sortBy: 'most-liked',
     resolution: 'standard_resolution',
-    template: '<a href="{{link}}" class="overlay red" style="background-image:url({{image}})"><div class="likes">&hearts; {{likes}}</div></a>'
+    template: '<a href="{{link}}" class="overlay" style="background-image:url({{image}})"><div class="likes">&hearts; {{likes}}</div></a>'
   });
   feed.run();
 })(jQuery);
