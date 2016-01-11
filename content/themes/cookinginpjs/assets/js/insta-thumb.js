31c594335f9717fe1ac713d311e3f5399fbc1d05
Sumedha <pramods@umich.edu>
2016-01-10 21:23:44 -0800

    makign more insta fixes. images still have white border

--- content/themes/cookinginpjs/assets/js/insta-thumb.js
+++ content/themes/cookinginpjs/assets/js/insta-thumb.js
@@ -4,9 +4,8 @@
     userId: 2462667315,
     accessToken: '2462667315.467ede5.6a8150827314439e9d18ba4c52596886',
     sortBy: 'most-liked',
-    resolution: 'standard_resolution',
+    resolution: 'thumbnail',
     template: '<a href="{{link}}" class="overlay" ><img src="{{image}}"><div class="likes"><i class="ion-ios-heart"></i>{{likes}} <i class="ion-chatbubble"></i> {{comments}}</div></a>'
   });
   feed.run();
 })(jQuery);
-
